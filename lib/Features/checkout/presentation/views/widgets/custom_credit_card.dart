864c8f8db5457e18ad713cc352a9aeec2a794cb5
tasneemakhaled <khaledtasneem402@gmail.com>
2026-02-21 16:50:08 +0200

    create custom credit card

---
+++ lib/Features/checkout/presentation/views/widgets/custom_credit_card.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_credit_card/flutter_credit_card.dart';
+
+class CustomCreditCard extends StatefulWidget {
+  const CustomCreditCard({super.key,});
+
+  @override
+  State<CustomCreditCard> createState() => _CustomCreditCardState();
+}
+
+class _CustomCreditCardState extends State<CustomCreditCard> {
+ String cardNumber='';
+ String expiryDate='';
+ String cardHolderName='';
+ String cvvCode='';
+bool showBackView=false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CreditCardWidget(cardNumber: cardNumber, expiryDate: expiryDate, cardHolderName: cardHolderName, cvvCode: cvvCode, showBackView: showBackView, onCreditCardWidgetChange: (value){})
+      ],
+    );
+  }
+}
--- lib/Features/checkout/presentation/views/widgets/payment_details_body.dart
+++ lib/Features/checkout/presentation/views/widgets/payment_details_body.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_payement_checkout/Features/checkout/presentation/views/widgets/payement_method_item.dart';
+import 'package:flutter_payement_checkout/Features/checkout/presentation/views/widgets/custom_credit_card.dart';
 import 'package:flutter_payement_checkout/Features/checkout/presentation/views/widgets/payement_methods_list_view.dart';
 
 class PaymentDetailsBody extends StatelessWidget {
@@ -10,14 +10,7 @@ class PaymentDetailsBody extends StatelessWidget {
     return Column(
       children: [
         PayementMethodsListView(),
-    //     Row(
-          
-    //       children: [
-    //  PaymentMethodItem(image: 'assets/images/credit_card.svg',isActive: true,),
-    //  SizedBox(width: 30,),
-    //       PaymentMethodItem(isActive: false, image: 'assets/images/paypal.svg')
-    //       ],
-    //     )
+       CustomCreditCard(),
       ],
     );
   }
