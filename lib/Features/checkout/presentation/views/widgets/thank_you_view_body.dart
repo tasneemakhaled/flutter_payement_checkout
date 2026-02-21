205ac975c551af5e33854a9d6b27a0458fb2ca98
tasneemakhaled <khaledtasneem402@gmail.com>
2026-02-22 01:18:16 +0200

    custom shaped container done

--- lib/Features/checkout/presentation/views/widgets/thank_you_view_body.dart
+++ lib/Features/checkout/presentation/views/widgets/thank_you_view_body.dart
@@ -5,6 +5,37 @@ class ThankYouViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.all(32.0),
+      child: Stack(
+        clipBehavior: Clip.none,
+        children: [
+        
+          Container(
+             
+            decoration: ShapeDecoration(
+                 color: Color(0xffD9D9D9),
+              shape: RoundedRectangleBorder(borderRadius: BorderRadiusGeometry.circular(20))),
+          ),
+          Positioned(
+            bottom: MediaQuery.of(context).size.height*.2,
+            left: -20,
+            child: CircleAvatar(backgroundColor: Colors.white,)),
+              Positioned(
+            bottom: MediaQuery.of(context).size.height*.2,
+            right: -20,
+            child: CircleAvatar(backgroundColor: Colors.white,)),
+           Positioned(
+            left: 0,
+            right: 0,
+            top: -50,
+            child: CircleAvatar(
+              radius: 50, 
+              backgroundColor: Color(0xffD9D9D9),child: CircleAvatar(
+                radius: 40,
+              backgroundColor: Color(0xff34A853),child: Icon(Icons.check,color: Colors.white,size: 50,),),))
+        ],
+      ),
+    );
   }
 }
