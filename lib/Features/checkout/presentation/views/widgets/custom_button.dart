f7d7877f7546c38e2fa7741c241ed845b86b966b
tasneemakhaled <khaledtasneem402@gmail.com>
2026-02-25 00:36:03 +0200

    finish ui done

--- lib/Features/checkout/presentation/views/widgets/custom_button.dart
+++ lib/Features/checkout/presentation/views/widgets/custom_button.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_payement_checkout/Features/checkout/presentation/views/payment_details_view.dart';
+import 'package:flutter_payement_checkout/Features/checkout/presentation/views/widgets/payement_methods_bottom_sheet.dart';
 import 'package:flutter_payement_checkout/core/utils/styles.dart';
 
 class CustomButton extends StatelessWidget {
@@ -17,9 +18,12 @@ class CustomButton extends StatelessWidget {
         ),
         
         onPressed: (){
-          Navigator.of(context).push(MaterialPageRoute(builder: (context){
-            return PaymentDetailsView();
-          }));
+          // Navigator.of(context).push(MaterialPageRoute(builder: (context){
+          //   return PaymentDetailsView();
+          // }));
+          showModalBottomSheet(context: context, builder: (context){
+            return PayementMethodsBottomSheet();
+          });
         }, child: Text('Complete Payement',style: Styles.style22,),),
     );
   }
---
+++ lib/Features/checkout/presentation/views/widgets/payement_methods_bottom_sheet.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_payement_checkout/Features/checkout/presentation/views/widgets/payement_methods_list_view.dart';
+import 'package:flutter_payement_checkout/core/utils/styles.dart';
+
+class PayementMethodsBottomSheet extends StatelessWidget {
+  const PayementMethodsBottomSheet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0,vertical: 16),
+      child: Column(
+       mainAxisSize: MainAxisSize.min,
+        children: [
+          PayementMethodsListView(),
+         SizedBox(height: 20,),
+          SizedBox(
+        width: double.infinity,
+        height: 60,
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Color(0xff34a853),
+            shape: RoundedRectangleBorder(borderRadius: BorderRadiusGeometry.circular(16))
+          ),
+          
+          onPressed: (){
+          
+          }, child: Text('Continue',style: Styles.style22,),),
+      ),
+        ],
+      ),
+    );
+  }
+}
