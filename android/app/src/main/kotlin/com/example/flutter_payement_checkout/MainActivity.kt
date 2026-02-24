67dd12ec6aae386c462e29861e657074ce74a84b
tasneemakhaled <khaledtasneem402@gmail.com>
2026-02-25 01:33:48 +0200

    finish setting of anroid for stripe

--- android/app/src/main/kotlin/com/example/flutter_payement_checkout/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_payement_checkout/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.example.flutter_payement_checkout
 
 import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
+import io.flutter.embedding.android.FlutterFragmentActivity
+class MainActivity : FlutterFragmentActivity()
