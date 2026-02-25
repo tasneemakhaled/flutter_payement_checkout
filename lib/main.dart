493ea22b03645398775e5cc872350513042c24d6
tasneemakhaled <khaledtasneem402@gmail.com>
2026-02-25 03:15:13 +0200

    steps for stripe payment

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_payement_checkout/Features/checkout/presentation/views/my_cart_view.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
 
-void main() {
+void main() async {
+  Stripe.publishableKey= "pk_test_51T4UMrKyNg9IW7Jaodnyp0omH6KZI3wopzktU8wwA5kV3N73JhafupXYy0MBALfbuFUr4t9sjvumYsi7MQfmxYH900DX1Vk8O7";
   runApp(const CheckoutApp());
 }
 
@@ -15,3 +17,31 @@ class CheckoutApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: MyCartView(),
     );}}
+// Future<void> initPaymentSheet() async {
+//   try {
+   
+   
+
+//     // 3. Initialize the payment sheet
+//     await Stripe.instance.initPaymentSheet(
+//       paymentSheetParameters: SetupPaymentSheetParameters(
+//         // Main params
+//         paymentIntentClientSecret: data['paymentIntent'],
+//         merchantDisplayName: 'Flutter Stripe Store Demo',
+       
+//         // Return URL for redirect-based payment methods
+//         returnURL: 'flutterstripe://redirect',
+        
+//       ),
+//     );
+//   } catch (e) {
+//     ScaffoldMessenger.of(context).showSnackBar(
+//       SnackBar(content: Text('Error: $e')),
+//     );
+//     rethrow;
+//   }
+// }
+
+    //1- create payment intent (amount,currency)
+    //2- init payment sheet (paymentIntentClientSecret,merchantDisplayName)
+    // present payment sheet
