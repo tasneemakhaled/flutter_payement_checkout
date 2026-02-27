e0f327d9d720c59228da9119dd79e44231697790
tasneemakhaled <khaledtasneem402@gmail.com>
2026-02-27 04:25:22 +0200

    create checkout cubit

---
+++ lib/Features/checkout/presentation/manager/cubit/payement_cubit.dart
@@ -0,0 +1,24 @@
+import 'dart:developer';
+
+import 'package:bloc/bloc.dart';
+import 'package:flutter_payement_checkout/Features/checkout/data/models/payment_intent_input_model.dart';
+import 'package:flutter_payement_checkout/Features/checkout/data/repos/checkout_repo.dart';
+import 'package:meta/meta.dart';
+
+part 'payement_state.dart';
+
+class PayementCubit extends Cubit<PayementState> {
+  PayementCubit(this.checkoutRepo) : super(PayementInitial());
+  final CheckoutRepo checkoutRepo;
+  Future makePayement({required PaymentIntentInputModel payementIntentInputModel })async{
+    emit(PayementLoading());
+   var data= await checkoutRepo.makePayement(payementIntentInputModel: payementIntentInputModel);
+   data.fold((l){emit(PayementFailure(errorMessage: l.errorMessage));}, (r)=>emit(PayementSuccess()));
+   
+  }
+  @override
+  void onChange(Change<PayementState> change) {
+   log(change.toString());
+    super.onChange(change);
+  }
+}
---
+++ lib/Features/checkout/presentation/manager/cubit/payement_state.dart
@@ -0,0 +1,14 @@
+part of 'payement_cubit.dart';
+
+@immutable
+sealed class PayementState {}
+
+final class PayementInitial extends PayementState {}
+final class PayementLoading extends PayementState {}
+final class PayementSuccess extends PayementState {}
+final class PayementFailure extends PayementState {
+  final String errorMessage;
+
+  PayementFailure({required this.errorMessage});
+
+}
