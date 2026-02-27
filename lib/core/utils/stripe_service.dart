29fe317589b1c8ae24ece740a578ecff88e10e72
tasneemakhaled <khaledtasneem402@gmail.com>
2026-02-27 03:11:54 +0200

    implement init payement sheet

--- lib/core/utils/stripe_service.dart
+++ lib/core/utils/stripe_service.dart
@@ -2,14 +2,19 @@ import 'package:flutter_payement_checkout/Features/checkout/data/models/payment_
 import 'package:flutter_payement_checkout/Features/checkout/data/models/payment_intent_model/payment_intent_model.dart';
 import 'package:flutter_payement_checkout/core/utils/api_keys.dart';
 import 'package:flutter_payement_checkout/core/utils/api_service.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
 
 class StripeService {
   final ApiService apiService=ApiService();
- Future< PaymentIntentModel >createPayemntIntent({required PaymentIntentInputModel paymentIntentInputModel})async{
+ Future<PaymentIntentModel>createPayemntIntent({required PaymentIntentInputModel paymentIntentInputModel})async{
  var response =await apiService.post(url: 'https://api.stripe.com/v1/payment_intents', token: ApiKeys.secretKey, body: paymentIntentInputModel.toJson());
   var  paymentIntentModel=PaymentIntentModel.fromJson(response.data);
    return paymentIntentModel;
   }
+Future initPayementSheet({required String paymentIntentClientSecret }) async{
+    Stripe.instance.initPaymentSheet(paymentSheetParameters: SetupPaymentSheetParameters(paymentIntentClientSecret:paymentIntentClientSecret,merchantDisplayName: 'tasneem' ));
+}
+
 }
 
 //1- create payment intent (amount,currency)
